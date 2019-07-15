7dac52c1b33d1765e32b835aa8627c3d396bd943
Jonas Bushart <jonas@bushart.org>
2019-07-15 16:11:58 +0200

    Provide a simple template for LaTeX papers including CI
    This project focusses on providing a template for managing LaTeX
    documents, including linting and building them in a CI environment.
    
    The project includes at least these parts:
    
    * A simple Makefile to build and zip all source files. It build the TeX
    files using `latexmk`.
    * Images are automatically converted to pdf.
    Supported source formats are SVG (via inkscape, place them into the
    figs/ folder) and odg/fodg files (via libreoffice, place them into the
    figs/raw/ folder).
    * The reposity comes with a set of pre-commit rules. These rules manage
    some style rules for the LaTeX code and also style rules for the
    pre-commit rules themselves.
    Style rules for LaTeX include things such as consistent spelling (e.g.,
    naive without double dots), or that "et al." has no emph around it.
    * Lastly, it comes with a pre-written CI pipeline for Gitlab, which
    builds the document and uploads it as a build artifact.
    The CI system also runs the pre-commit and some additional checks, to
    make sure common and easily detectable errors do not slip through.

---
+++ .ci/sanity-checks.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import inspect
+import re
+import sys
+import typing as t
+from collections import Counter
+from enum import Enum
+from glob import glob
+from textwrap import dedent, wrap
+
+
+class ExitStatus(Enum):
+    SUCCESS = 0
+    ERROR = 1
+
+
+PROGRAM_STATUS = ExitStatus.SUCCESS
+RE_URLS = re.compile(r"(?:ftp|https?)://[^\}]*")
+
+
+def print_description() -> None:
+    """
+    Print the docstring of the calling function, but at most once.
+    """
+    frame = inspect.currentframe()
+
+    caller_frame = inspect.getouterframes(frame)[1][0]
+    caller_name = inspect.getframeinfo(caller_frame).function
+    caller_func = eval(caller_name)
+    try:
+        if caller_func.warning_already_printed == True:
+            return
+    except AttributeError:
+        caller_func.warning_already_printed = True
+        doc = dedent(caller_func.__doc__.strip("\n"))
+        # Textwrap cannot really wrap paragraphs correctly.
+        # So we first split the text into multiple paragraphs and wrap them individually.
+        for paragraph in doc.split("\n\n"):
+            for line in wrap(paragraph):
+                print(line)
+            print()
+
+
+def check_for_duplicate_urls_in_references(files: t.List[t.AnyStr]) -> None:
+    """
+    Some references point to the same URL.
+
+    This can happen either because two references share the same URL, in this case consider merging the references.
+    Another option is, that the current bibtex style supports both `url` and `note` fields and prints both URLs. Consider removing the `note` field.
+    """
+    global PROGRAM_STATUS
+    for file in files:
+        with open(file, "rt") as f:
+            content = f.read()
+
+        urls = Counter(map(sanitize_url, RE_URLS.findall(content)))
+        for url, count in sorted(urls.items()):
+            if count > 1:
+                print_description()
+                print(f"Duplicate: {url}\n    The URL occurs {count} times.")
+                PROGRAM_STATUS = ExitStatus.ERROR
+
+
+def sanitize_url(url: str) -> str:
+    """
+    This function strips to the protocol, e.g., http, from urls.
+
+    This ensures that URLs can be compared, even with different protocols, for example, if both http and https is used.
+    """
+    prefixes = ["https", "http", "ftp"]
+    for prefix in prefixes:
+        if url.startswith(prefix):
+            url = url[len(prefix) :]
+    return url
+
+
+def main() -> None:
+    check_for_duplicate_urls_in_references(glob("*.bbl"))
+
+    if PROGRAM_STATUS == ExitStatus.SUCCESS:
+        print("All checks passed!")
+        sys.exit(0)
+    elif PROGRAM_STATUS == ExitStatus.ERROR:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Makefile
@@ -0,0 +1,56 @@
+SVGS=$(wildcard figs/*.svg)
+SVGS_TO_PDF=$(SVGS:.svg=.pdf)
+
+ODGS=$(wildcard figs/raw/*.odg)
+ODGS_TO_PDF=$(ODGS:figs/raw/%.odg=figs/%.pdf)
+FODGS=$(wildcard figs/raw/*.fodg)
+FODGS_TO_PDF=$(FODGS:figs/raw/%.fodg=figs/%.pdf)
+
+figs_CONVERTED=$(SVGS_TO_PDF)
+figs_CONVERTED+=$(ODGS_TO_PDF)
+figs_CONVERTED+=$(FODGS_TO_PDF)
+
+# Crop PDFs to the drawing area and removes empty whitespace or transparent areas
+define pdfcrop
+	pdfcrop --margin 0 $@ $@
+endef
+
+# Use libreoffice to convert ODG graphics into PDFs
+define odg_to_pdf
+	soffice -env:UserInstallation=file:///tmp/libreoffice-cli/ --convert-to pdf --headless $< --outdir figs/
+	$(pdfcrop)
+endef
+
+# default target
+all: figs citations pdf zip
+
+pdf: *.tex
+	latexmk -pdf -pdflatex="pdflatex --shell-escape %O %S" paper.tex
+
+figs: $(figs_CONVERTED)
+
+.PHONY: clean
+clean:
+	rm -f $(figs_CONVERTED) sources.zip paper.pdf
+
+# convert svg to pdf
+figs/%.pdf: figs/%.svg
+	inkscape --export-area-drawing --without-gui --file=$< --export-pdf=$@
+	$(pdfcrop)
+
+# convert (f)odg to pdf
+figs/%.pdf: figs/raw/%.odg
+	$(odg_to_pdf)
+figs/%.pdf: figs/raw/%.fodg
+	$(odg_to_pdf)
+
+.PHONY: citations
+citations: $(wildcard *.bib)
+# Guard against non-existing bib files
+	@if [ "$^" != "" ]; then \
+		sed -i 's/^\(\s*month\s*=\s*\)[{]\([^M]..\)[}]/\1\2/g' $^; \
+	fi
+
+.PHONY: zip
+zip:
+	zip -r sources.zip *.tex *.bib *.cls *.bst *.sty figs/*.pdf figs/*.svg
